5f4c3390ba902d78f2b0d875a7d72d07c05e4dec
Ahmad Moltafet <55408986+amoltafet@users.noreply.github.com>
2025-11-08 05:24:38 +0000

    ooo

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,41 @@ import { PlayerContext } from "./context/PlayerContext"
 import { StatsigProvider, useClientAsyncInit } from '@statsig/react-bindings';
 import { StatsigAutoCapturePlugin } from '@statsig/web-analytics';
 import { StatsigSessionReplayPlugin } from '@statsig/session-replay';
+import { useEffect } from "react";
+import { GrowthBookProvider } from "@growthbook/growthbook-react";
+import { GrowthBook } from "@growthbook/growthbook";
+import { 
+  thirdPartyTrackingPlugin,
+  autoAttributesPlugin
+} from "@growthbook/growthbook/plugins";
+
+const growthbook = new GrowthBook({
+  apiHost: "https://cdn.growthbook.io",
+  clientKey: "sdk-KZbH7FJfdS0iNTcW",
+  enableDevMode: true,
+  plugins: [
+    autoAttributesPlugin(),
+    thirdPartyTrackingPlugin({ trackers: ["ga4", "gtm"] }),
+  ],
+});
 
 const App = () => {
-  const {audioRef,track} = useContext(PlayerContext)
-  const { client } = useClientAsyncInit(
-    "client-QSXaobm2DKxdOXm1fukCT7kWcBrPMlrFWNZ7T1wicCm",
-    { userID: 'a-user' }, 
-    { plugins: [ new StatsigAutoCapturePlugin(), new StatsigSessionReplayPlugin() ] },
-  );
+  // const {audioRef,track} = useContext(PlayerContext)
+  // const { client } = useClientAsyncInit(
+  //   "client-QSXaobm2DKxdOXm1fukCT7kWcBrPMlrFWNZ7T1wicCm",
+  //   { userID: 'a-user' }, 
+  //   { plugins: [ new StatsigAutoCapturePlugin(), new StatsigSessionReplayPlugin() ] },
+  // );
+
+    useEffect(() => {
+      // Load features asynchronously when the app renders
+      growthbook.init({ streaming: true });
+    }, []);
+
 
   return (
-    <StatsigProvider client={client} loadingComponent={<div>Loading...</div>}>
+   <GrowthBookProvider growthbook={growthbook}>
+
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar/>
@@ -27,7 +51,7 @@ const App = () => {
 
       </audio>
     </div>
-    </StatsigProvider>
+    </GrowthBookProvider>
   )
 }
 
